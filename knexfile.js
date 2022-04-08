c1f9c8610942b92aa6f4e99cc101015d2b7af671
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-04-08 14:48:07 +0300

    initial knex and pg setup
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

---
+++ knexfile.js
@@ -0,0 +1,96 @@
+// Update with your config settings.
+const path = require('path');
+const BASE_PATH = path.join(__dirname, 'db');
+let connections;
+
+try {
+  connections = require('./config/db');
+} catch (e) {
+  connections = require('./config/db.example');
+}
+
+const {
+  POSTGRES_USER: user,
+  POSTGRES_DB: database,
+  POSTGRES_HOST: host,
+  POSTGRES_PASSWORD: password,
+  POSTGRES_PORT: port
+} = process.env;
+let env_credentials;
+
+if (user && host && database && password) {
+  env_credentials = {
+    user,
+    database,
+    host,
+    password,
+    port
+  };
+}
+
+module.exports = {
+  ci: {
+    client: 'pg',
+    connection: {
+      port: process.env.PG_PORT,
+      host: process.env.PG_HOST,
+      database: process.env.PG_NAME,
+      user: process.env.PG_USER
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  action: {
+    client: 'pg',
+    connection: env_credentials || connections.action,
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: env_credentials || connections.test,
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  development: {
+    debug: true,
+    client: 'pg',
+    connection: env_credentials || connections.development,
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: env_credentials || connections.production,
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    },
+    log: {
+      error(message) {
+        console.log(message);
+      },
+      debug(message) {
+        console.log(message);
+      },
+    }
+  }
+};
---
+++ src/config/db.example.js
@@ -0,0 +1,28 @@
+module.exports = {
+    action: {
+      user: 'postgres',
+      password: 'postgres',
+      database: 'postgres'
+    },
+    test: {
+      host: 'localhost',
+      database: 'strapi_test',
+      user: 'strapi',
+      password: 'password',
+      port: '5432'
+    },
+    development: {
+      host: 'localhost',
+      database: 'strapi_dev',
+      user: 'strapi',
+      password: 'password',
+      port: '5432'
+    },
+    production: {
+      host: 'localhost',
+      database: 'strapi',
+      user: 'strapi',
+      password: 'password',
+      port: '5432'
+    }
+  };
