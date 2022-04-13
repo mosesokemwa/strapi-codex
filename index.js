eb749b7ba92dfd302b8a4c6122987e50c923a9cd
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-04-13 03:15:38 +0300

    code + project cleanup
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- index.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const app = require('./src/server');
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, _ => console.log(`Server is running on port ${PORT} on ${process.env.NODE_ENV}`));
